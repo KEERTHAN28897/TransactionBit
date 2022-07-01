9333409191fdedd4e129225e66b43f59976910c6
KEERTHAN28897 <40045500+KEERTHAN28897@users.noreply.github.com>
2022-07-02 00:45:41 +0530

    Updating main

--- P9/src/main/java/Main.java
+++ P9/src/main/java/Main.java
@@ -1,11 +1,11 @@
 import accessor.AncestryTree;
-import controller.ConnectionRequest;
 
 import java.io.IOException;
 
 public class Main{
 
     public static void main(String[] args) throws IOException {
-        AncestryTree.fetch();
+        AncestryTree ancestryTree = new AncestryTree();
+       System.out.println(ancestryTree.getTopTenTransactions());
     }
 }
